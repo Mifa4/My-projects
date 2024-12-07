3ebffd5d39cad01b26c566eb849a5bbc4199e464
ingener2010 <167018345+Mifa4@users.noreply.github.com>
2024-12-07 15:46:34 +0400

    try to stop process

--- MusicPlayer/player.py
+++ MusicPlayer/player.py
@@ -1,11 +1,18 @@
 import threading
 import pyglet
 path = input('Where is the project? example: C:\\Users\\You\\Documents\\GitHub\\My-projects\\My-projects\\MusicPlayer \n') + '\\musics\\'
-
+pathSave = path
 path += input('Имя файла в папке music ((example: test) automaticly add .mp3):\t') + '.mp3'
 def Player(path):
     song = pyglet.media.load(path)
     song.play()
-mainPlayerThr = threading.Thread(target=Player,name='Player',args=(path,))
+    pyglet.app.run()
+mainPlayerThr = threading.Thread(target=Player,name='Player',args=(path,),daemon=True)
 mainPlayerThr.start()
-pyglet.app.run()
+
+while True:
+    do = input()
+    if(do == 'exit'):
+        pyglet.app.exit()
+        mainPlayerThr.join()
+
