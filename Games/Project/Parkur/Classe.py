806ff603c37bead304bf651b88ee04181f496400
Mifsi24 <167018345+Mifa4@users.noreply.github.com>
2025-05-20 21:51:20 +0400

    NNN

--- Games/Project/Parkur/Classe.py
+++ Games/Project/Parkur/Classe.py
@@ -1,6 +1,9 @@
 import pygame
 from Parkur.Methods  import *
 import Parkur.Methods
+from Story.Console_of_secrets import *
+import Story.Console_of_secrets
+import random
 
 pygame.init()
 class Object:
@@ -21,7 +24,7 @@ class Object:
         self.type = type
         self.p = False
     
-    def Update(self,keys,obj,level,objects,offIndex):
+    def Update(self,keys,obj,level,objects,offIndex,thisObj):
         if self.falling == False:
             if keys[pygame.K_w] or keys[pygame.K_UP]:
                 self.count += self.Jump()
@@ -37,9 +40,13 @@ class Object:
         elif self.type == 'Obctacle':
             self.Obctacle(obj)
         elif self.type == 'Button':
-            self.Button(obj,offIndex,objects)
+            self.Button(obj,offIndex,objects,thisObj)
+        elif self.type == 'Teleport':
+            self.Teleport(obj)
         elif self.type == 'End':
             self.NextLevel(Parkur.Main.Player,level)
+        elif self.type == 'Over':
+            self.End(obj)
         self.rect = self.image.get_rect()
         
 
@@ -90,6 +97,31 @@ class Object:
         selfleft = self.pos[0]
         #if bottom <= selftop and right >= selfleft and left <= selfright and top >= selfbottom:
         if selfbottom >= top and selfright >= left+10 and selfleft <= right-10 and selftop <= bottom-10:
+            Parkur.Main.deaths += 1
+            s = random.randint(0,10)
+            if s == 0:
+                Say('[???]:...',0.1)
+            elif s == 1:
+                Say('[???]:Это просто!',0.1)
+            elif s == 2:
+                Say('[???]:Ты полный #####!',0.1)
+            elif s == 3:
+                Say('[???]:Ты специально?',0.1)
+            elif s == 4:
+                Say('[???]:Хватит это делать!',0.1)
+            elif s == 5:
+                Say('[???]:Ещё один ту####!!!',0.1)
+            elif s == 6:
+                Say('[???]:???',0.1)
+            elif s == 7:
+                Say('[???]: :((((((((((((((((((((',0.1)
+            elif s == 8:
+                Say('[???]:wwwaaaaaaaaa',0.1)
+            elif s == 9:
+                Say('[???]:мое терпение кончаеться!',0.1)
+            elif s == 10:
+                pass
+
             obj.pos = obj.start_pos[:]
     
     def Button(self,obj,offObjIndex,objects,thisObj):
@@ -106,6 +138,19 @@ class Object:
             objects.pop(offObjIndex)
             objects.pop(thisObj)
     
+    def Teleport(self,obj):
+        top = obj.pos[1]
+        bottom = obj.pos[1] + obj.size[1]
+        right = obj.pos[0] + obj.size[0]
+        left = obj.pos[0]
+        selftop = self.pos[1]
+        selfbottom = self.pos[1] + self.size[1]
+        selfright = self.pos[0] + self.size[0]
+        selfleft = self.pos[0]
+        #if bottom <= selftop and right >= selfleft and left <= selfright and top >= selfbottom:
+        if selfbottom >= top and selfright >= left+10 and selfleft <= right-10 and selftop <= bottom-10:
+            obj.pos = [50,50]
+    
     def NextLevel(self,obj,level):
         top = obj.pos[1]
         bottom = obj.pos[1] + obj.size[1]
@@ -118,6 +163,38 @@ class Object:
         #if bottom <= selftop and right >= selfleft and left <= selfright and top >= selfbottom:
         if selfbottom >= top and selfright >= left and selfleft <= right and selftop <= bottom:
             Parkur.Methods.New_Level()
+            if level == 1:
+                Say('[???]:Тебе еще надо много пройти.',0.1)
+                Parkur.Main.musics.Play(1)
+            elif level == 2:
+                Say('[???]:2! Мало кто это проходил!',0.1)
+                Parkur.Main.musics.Play(2)
+            elif level == 3:
+                Say('[???]:Ты почти добрался до меня.',0.1)
+                Parkur.Main.musics.Play(3)
+            elif level == 4:
+                Say('[???]:Ещё чуть-чуть.',0.1)
+                Say('[???]:Ты почти попал в моё начало!.!',0.1)
+                Parkur.Main.musics.Play(4)
+            Parkur.Main.sec,Parkur.Main.min,Parkur.Main.hours = 0,0,0
+    
+    def End(self,obj):
+        top = obj.pos[1]
+        bottom = obj.pos[1] + obj.size[1]
+        right = obj.pos[0] + obj.size[0]
+        left = obj.pos[0]
+        selftop = self.pos[1]
+        selfbottom = self.pos[1] + self.size[1]
+        selfright = self.pos[0] + self.size[0]
+        selfleft = self.pos[0]
+        #if bottom <= selftop and right >= selfleft and left <= selfright and top >= selfbottom:
+        if selfbottom >= top and selfright >= left and selfleft <= right and selftop <= bottom:
+            Say('[VirtualKiller]:Спасибо!!!!!!!!!!!!!!!!!!!!!!!.',0.15)
+            Say('[Console]: Объект выбрался на поверхность!',0.001)
+            Say('[Console]: Объект был потерян!',0.001)
+            Say('![Console]!: Произошла фотальная ошибка. ERR0R',0.7)
+            Parkur.Main.Play = False
+            Err0r()
 
 class Text:
     def __init__(self,name,size,text,pos,color):
@@ -126,15 +203,9 @@ class Text:
         self.pos = pos
         self.color = color
     
-    def Render(self):
-        self.Font.render(self.text,True,(self.color[0],self.color[1],self.color[2]))
-
-class Level:
-    def __init__(self,size,color,posOnMainSurface,Surface):
-        self.level = pygame.Surface((size[0],size[1]))
-        self.level.fill((color[0],color[1],color[2]))
-        self.pos = posOnMainSurface
-        Surface.blit(self.level,(self.pos[0],self.pos[1]))
+    def Render(self,win):
+        Render = self.Font.render(str(self.text),True,(self.color[0],self.color[1],self.color[2]))
+        win.blit(Render,(self.pos[0],self.pos[1]))
 
 class MusicPlayer:
     def __init__(self,musics_path):
@@ -143,6 +214,6 @@ class MusicPlayer:
             self.musics.append(pygame.mixer.Sound(i))
     
     def Play(self,music_index):
-        for i in range(0,len(self.bools)):
+        for i in range(0,len(self.musics)):
             self.musics[i].stop()
         self.musics[music_index].play()
--- Games/Project/Parkur/Main.py
+++ Games/Project/Parkur/Main.py
@@ -1,26 +1,70 @@
 import pygame
+import random
+import threading
 from Parkur.Methods import *
 import Parkur.Methods
 from Parkur.Classe import *
 import Parkur.Classe
 Player = Object([50,50],[27,327], [27, 327],r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Textures\PlayerT1.png',r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Textures\PlayerT2.png',True,'')
 level = 1
+sec,min,hours = 0,0,0
+deaths = 0
+tr = threading.Thread(target=Timer)
+tr.start()
+musics = MusicPlayer([r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Musics\Level1.mp3',r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Musics\Level2.mp3',r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Musics\Level3.mp3',r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Musics\Level4.mp3',r'C:\Users\Елена\Documents\GitHub\My-projects\Games\Project\Parkur\Musics\Level5.mp3'])
+Play = True
 def Parkur_Time():
-    global Player,level
+    global Player,level,sec,min,hours,Play
     pygame.init()
     w,h = 1000,500
     win = pygame.display.set_mode((w,h))
     FPS = 60
     clock = pygame.time.Clock()
-    while True:
+    if level == 1:
+        musics.Play(0)
+    while Play:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 exit()
-        win.fill((255,255,255))
+                location_Text = Text(None,50,f'Локация:',(500,0),(255,50,50))
+        if level == 1:
+            win.fill((255,255,255))
+            location_Text = Text(None,50,f'Локация: Вход к [???]',(500,0),(255,50,50))
+        elif level == 2:
+            location_Text = Text(None,50,f'Локация: Хоол',(500,0),(255,50,50))
+            win.fill((200,200,200))
+        elif level == 3:
+            location_Text = Text(None,50,f'Локация: Запретная секция',(500,0),(255,50,50))
+            win.fill((100,100,100))
+        elif level == 4:
+            location_Text = Text(None,50,f'Локация: Падение',(500,0),(255,50,50))
+            win.fill((50,50,50))
+        elif level == 5:
+            location_Text = Text(None,25,f'!Controller of corect work!: Не открывай!!!',(255,0),(255,50,50))
+            win.fill((random.randint(0,50),random.randint(0,50),random.randint(0,50)))
+        if not pygame.mixer.get_busy():
+            if level == 1:
+                Parkur.Main.musics.Play(0)
+            elif level == 2:
+                Parkur.Main.musics.Play(1)
+            elif level == 3:
+                Parkur.Main.musics.Play(2)
+            elif level == 4:
+                Parkur.Main.musics.Play(3)
+            elif level == 5:
+                Parkur.Main.musics.Play(4)
+        DeathCount_Text = Text(None,25,f'Смерти: {deaths}',(450,55),(255,255,50))
+        level_Text = Text(None,50,f'Уровень: {level}',(0,0),(50,255,50))
+        Timer_Text = Text(None,25,f'Время: {sec}:{min}:{hours}',(0,60),(50,255,255))
         keys = pygame.key.get_pressed()
         Player.Mouve(5,keys)
         Player.Update(keys,'',level,None,None,None)
         win.blit(Player.image,(Player.pos[0],Player.pos[1]))
         Load_Level(level,win,Player)
+        level_Text.Render(win)
+        location_Text.Render(win)
+        Timer_Text.Render(win)
+        DeathCount_Text.Render(win)
         pygame.display.update()
         clock.tick(FPS)
+    exit()
